0a5493313499ea57abd4dc10c24dda3598adb340
Pawel Bozemski <paw.boz@gmail.com>
2020-03-29 14:16:06 +0200

    Add models

--- server.js
+++ server.js
@@ -33,7 +33,6 @@ db.once('open', () => {
 });
 db.on('error', err => console.log('Error ' + err));
 
-
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
